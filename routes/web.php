18c370f5092240acca73959b6e70d6e450e52e5a
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-10-22 15:01:28 -0300

    Se puede crear un user credential mediante la ruta /register

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\UserCredential;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
    return view('welcome');
@@ -11,4 +12,19 @@ Route::get('/', function () {
 Route::get('/a', function(Request $r)
 {
    return 'HOLA';
+});//->middleware('web');
+
+Route::post('/register', function(Request $r)
+{
+   $newAuthData = new UserCredential();
+
+   $newAuthData->email = $r->email;
+   $newAuthData->password = Hash::make($r->password);
+
+   $newAuthData->fill($r->all());
+
+   $estado = $newAuthData->save();
+
+
+   return ['a'=> 'HOLA', 'Estado'=> $estado, 'ALL' => $r->all()];
 });//->middleware('web');
